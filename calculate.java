b6f00ea851ee39aa922944f54e4af251a25b7af0
serega100500 <serega_100500@bk.ru>
2016-09-15 01:01:07 +0300

    Add sum

--- calculate.java
+++ calculate.java
@@ -1,5 +1,9 @@
 public class calculate {
 	public static void main (String[] arg) {
 		System.out.println("Calculate...");
+		int first = Integer.valueOf(arg[0]);
+		int second = Integer.valueOf(arg[1]);
+		int sum = first+second;
+		System.out.println("Sum "+sum);
 	}
 }
