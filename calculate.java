cbb2ef9bd6b660a9feb9fc81376f9712de667cc0
serega100500 <serega_100500@bk.ru>
2016-09-15 01:16:42 +0300

    Add new options

--- calculate.java
+++ calculate.java
@@ -1,9 +1,20 @@
+import java.lang.Math;
 public class calculate {
 	public static void main (String[] arg) {
 		System.out.println("Calculate...");
-		int first = Integer.valueOf(arg[0]);
-		int second = Integer.valueOf(arg[1]);
-		int sum = first+second;
-		System.out.println("Sum "+sum);
+		
+		int a = Integer.valueOf(arg[0]);
+		int b = Integer.valueOf(arg[1]);
+		int sum = a+b;
+		int sub = a-b;
+		double div = a/b;
+		int mult = a*b;
+		double power = Math.pow(a,b);
+		
+		System.out.println(a+"+"+b+"="+sum);
+		System.out.println(a+"-"+b+"="+sub);
+		System.out.println(a+"/"+b+"="+div);
+		System.out.println(a+"*"+b+"="+mult);
+		System.out.println(a+"^"+b+"="+power);
 	}
 }
